0fa2029119b4e74e1e7a586032df34867f3acb0c
kersyxer <lukinchuk.vladyslav@gmail.com>
2024-11-23 18:58:03 +0100

    GUI update function

--- GUI.py
+++ GUI.py
@@ -12,26 +12,27 @@ class SudokuGUI:
         # Встановлення розмірів вікна
         self.root.geometry(f"{WIDTH}x{HEIGHT}")
 
-
-    def create_grid(self):
         self.canvas = tk.Canvas(self.root, width=FIELD_RENDER_SIZE, height=FIELD_RENDER_SIZE, bg='WHITE')
         self.canvas.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
+        self.create_grid()
 
+
+    def create_grid(self):
         grid_size = self.game.size
         cell_size = FIELD_RENDER_SIZE / grid_size
 
         for i in range(grid_size + 1):
             # Горизонтальні лінії
-            self.canvas.create_line(50,150+ i * cell_size, FIELD_RENDER_SIZE+50, 150+i * cell_size, fill="black")
+            self.canvas.create_line(50,150+ i * cell_size, FIELD_RENDER_SIZE+50, 150+i * cell_size, fill="black", tags="grid")
             # Вертикальні лінії
-            self.canvas.create_line(50+i * cell_size, 150, 50+i * cell_size, 150+FIELD_RENDER_SIZE, fill="black")
+            self.canvas.create_line(50+i * cell_size, 150, 50+i * cell_size, 150+FIELD_RENDER_SIZE, fill="black", tags="grid")
 
         section_size = int(grid_size ** 0.5)  # Розмір секції (3 для 9×9, 2 для 4×4)
         for i in range(section_size, grid_size, section_size):
             # Товсті горизонтальні лінії
-            self.canvas.create_line(50, 150+i * cell_size, 50+FIELD_RENDER_SIZE, 150+i * cell_size, fill="black", width=2)
+            self.canvas.create_line(50, 150+i * cell_size, 50+FIELD_RENDER_SIZE, 150+i * cell_size, fill="black", width=2, tags="grid")
             # Товсті вертикальні лінії
-            self.canvas.create_line(50+i * cell_size, 150,50+ i * cell_size, 150+FIELD_RENDER_SIZE, fill="black", width=2)
+            self.canvas.create_line(50+i * cell_size, 150,50+ i * cell_size, 150+FIELD_RENDER_SIZE, fill="black", width=2, tags="grid")
         self.fill_grid()
 
     def fill_grid(self):
@@ -45,12 +46,21 @@ class SudokuGUI:
                 if value != 0:  # Якщо клітинка не порожня
                     x = col * cell_size + cell_size / 2 + 50
                     y = row * cell_size + cell_size / 2 + 150
-                    self.canvas.create_text(x, y, text=str(value), font=("Arial", FONT1_SIZE), fill="blue")
+                    self.canvas.create_text(x, y, text=str(value), font=("Arial", FONT1_SIZE), fill="blue", tags="grid")
 
+    def update_grid(self):
+        self.canvas.delete("grid")
+        self.create_grid()
 
 if __name__ == "__main__":
     root = tk.Tk()
     game = Game(FieldType.f9x9)
     app = SudokuGUI(root, game)
-    app.create_grid()
+
+    def change_game_field():
+        game.field[0][0] = 5  # Зміна першої клітинки
+        app.update_grid()  # Оновлення відображення
+
+    root.after(3000, change_game_field)
+
     root.mainloop()
